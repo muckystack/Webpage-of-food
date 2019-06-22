e173a3052105f33c2de496fed9deaba230aee616
MIKE <MiguelMartinezDW@outlook.com>
2019-06-21 23:39:41 -0500

    Created design of the menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/container/body/menu/menu.component';
+import { ContainerComponent } from './components/container/container.component';
+import { BodyComponent } from './components/container/body/body.component';
+import { ProductsComponent } from './components/container/body/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContainerComponent,
+    BodyComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
