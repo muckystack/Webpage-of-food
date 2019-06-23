184d218fe1c0d26b2b9bc4b50081496b3130dbd9
MIKE <MiguelMartinezDW@outlook.com>
2019-06-22 22:59:06 -0500

    end proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { ContainerComponent } from './components/container/container.component';
 import { DessertComponent } from './components/dessert/dessert.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     ContainerComponent,
-    DessertComponent
+    DessertComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/restaurant/restaurant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant',
+  templateUrl: './restaurant.component.html',
+  styleUrls: ['./restaurant.component.styl']
+})
+export class RestaurantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
